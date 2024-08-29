761edb1ab4f968673578a723c00b573d5b8f802e
Urbnauq <104640977+Urbnauq@users.noreply.github.com>
2024-08-29 09:35:12 -0400

    mac

--- player.py
+++ player.py
@@ -10,7 +10,7 @@ class Player(CircleShape):
         self.radius = PLAYER_RADIUS
 
 def draw(self, screen):
-    pygame.draw.polygon(screen, "white", self.triangle(), width=2)      
+    pygame.draw.polygon(screen, "white", self.triangle(), 2)      
 
 def triangle(self):
     forward = pygame.Vector2(0, 1).rotate(self.rotation)
