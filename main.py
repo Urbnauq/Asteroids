f1c65d32500cf054d262e758e99a0d6c670c4d51
Urbnauq <eduardoalmonte18@gmail.com>
2024-08-29 09:41:25 -0400

    Fixed

--- main.py
+++ main.py
@@ -1,42 +1,29 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout thi file 
-import pygame 
+import pygame
 from constants import *
 from player import Player
 
+
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-    player = Player(x, y)
-    
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return        
-        
+                return
+
         screen.fill("black")
-        player.draw(screen) 
-        pygame.display.flip()   
-        
-        # Limit FPS to 60
+        player.draw(screen)
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
-
-
-
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
